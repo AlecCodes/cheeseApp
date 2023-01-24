9dda83e6eb4600f19ce73776b867a0bf84ff5ab9
Alec Hannaford <alec322@gmail.com>
2023-01-24 14:47:12 -0800

    user controller

---
+++ controllers/user.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+const {Router} = require("express")
+const router = Router()
+const jwt = require('jsonwebtoken')
+const {secret} = process.env || 'bruh'
+
+router.post("/signup", async(req,res)=>{
+    try{
+    req.body.password = bcrypt.hash(req.body.password, 10)
+    const newUser = await User.create(req.body)
+    res.json(newUser)}
+    catch(error){
+        res.status(400).json({error})
+    }
+})
+
+router.post('/login', async(req,res) => {
+    try{
+        const {username, password} = req.body
+        const user = await User.findOne({username})
+        if (user){
+            const match = await bcrypt.compare(password, user.password)
+            if(match){
+                const token = await jwt.sign({username}, secret)
+            }else{
+                res.status(400).json({error: "password does not match"})
+            }
+        }else{
+            res.status(400).json({error: "USER DOES NOT EXIST"})
+        }
+    }
+    catch(error){
+        res.status(400).json({error})
+    }
+})
+
+module.exports = Router
---
+++ models/user.js
@@ -0,0 +1,9 @@
+const {Schema, model} = require("mongoose")
+const UserSchema = new Schema({
+    username: {type: String, required: true, unique:true},
+    password: {type: String, required: true}
+},{timestamps: true})
+
+const User = model("user", userSchema)
+
+module.exports = User;
