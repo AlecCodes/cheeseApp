30dbe406e9e17ffe8a8bede354a702d2e4b7bbda
Alec Hannaford <alec322@gmail.com>
2023-01-25 15:43:50 -0800

    authorization for index post and put routes

--- controllers/cheeses.js
+++ controllers/cheeses.js
@@ -1,31 +1,40 @@
 const Cheese = require('../models/cheese')
 const {Router} = require('express')
 const router = Router()
+const auth = require('../auth')
+
+
 
 router.get("/", (req,res)=>{
     res.json({message: "Cheese router"})
 })
 
 //INDEX
-router.get("/index", async (req,res)=>{
+router.get("/index", auth, async (req,res)=>{
     try{
-        res.json(await Cheese.find({}))
+        //this happens once user is authenticated
+        const {username} = req.payload
+        res.json(await Cheese.find({username}))
     } catch (error) {
         res.status(400).json(error)
     }
 })
 
 //POST route
-router.post("/", async(req,res)=>{
+router.post("/", auth, async(req,res)=>{
     try{
+        const {username} = req.payload
+        req.body.username = username
         res.json(await Cheese.create(req.body))
     } catch (error){
         res.status(400).json(error)
     }
 })
 //PUT Route
-router.put("/:id", async(req,res) => {
+router.put("/:id", auth, async(req,res) => {
     try{
+        const {username} = req.payload
+        req.body.username = username
         res.json(await Cheese.findByIdAndUpdate(req.params.id, req.body, {new:true}))
     }catch(error){
         res.status(400).json(error)
--- models/cheese.js
+++ models/cheese.js
@@ -1,6 +1,7 @@
 const {Schema, model} = require('mongoose')
 
 const CheeseSchema = new Schema({
+    username: {type: String, unique: true},
     name: String,
     image: String,
     country: String
