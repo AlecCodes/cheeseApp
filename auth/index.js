b92f0138ecc47274947b2bc39cc963c077588d04
Alec Hannaford <alec322@gmail.com>
2023-01-24 19:15:32 -0800

    functional auth middleware

--- auth/index.js
+++ auth/index.js
@@ -10,6 +10,13 @@ const auth = async (req, res, next) => {
     if (req.headers.authorization){
         const token = req.headers.authorization.split(" ")[1]
         const payload = await jwt.verify(token, SECRET)
+        if(payload){
+            req.payload = payload
+            next()
+
+        }else{
+            res.status(400).json({error: "Verification failed or no payload"})
+        }
     }else{
         res.status(400).json({error: "NO AUTH HEADER"})
     }}
@@ -17,3 +24,4 @@ const auth = async (req, res, next) => {
         res.status(400).json(error)
     }
 }
+module.exports = auth
--- server.js
+++ server.js
@@ -10,12 +10,18 @@ const PORT = process.env.PORT || 3001
 const {DATABASE_URL, NODE_ENV} = process.env
 const AuthRouter = require('./controllers/user')
 const CheeseRouter = require('./controllers/cheeses')
-
+const auth = require("./auth/index")
 
 //MIDDLEWARE
 app.use(express.json())
 app.use(morgan('dev'))
 // app.use(NODE_ENV === 'production' ? cors(corsOptions) : cors())
+
+//Routers
+app.get("/", auth, (req,res) => {
+    res.json(req.payload)
+})
+
 app.use('/auth', AuthRouter)
 app.use('/cheeses',CheeseRouter)
 //app.use(express.state("public"))
